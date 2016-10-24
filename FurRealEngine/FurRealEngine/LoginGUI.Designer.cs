db15e5fcbf357b118b1530aecb4fad1cc55a89f5
Ethan Wright <ethwrig@siue.edu>
2016-10-23 19:59:28 -0500

    Login Gui design is done.

--- FurRealEngine/FurRealEngine/LoginGUI.Designer.cs
+++ FurRealEngine/FurRealEngine/LoginGUI.Designer.cs
@@ -28,11 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(428, 309);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(428, 397);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(513, 472);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(428, 472);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.checkBox1.Location = new System.Drawing.Point(435, 434);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Remember Password?";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // LoginGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "LoginGUI";
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = global::FurRealEngine.Properties.Resources.FurRealEngine;
+            this.ClientSize = new System.Drawing.Size(995, 623);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoginGUI";
+            this.Text = "FurRealEngine";
+            this.Load += new System.EventHandler(this.LoginGUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- FurRealEngine/FurRealEngine/LoginGUI.cs
+++ FurRealEngine/FurRealEngine/LoginGUI.cs
@@ -12,9 +12,81 @@ namespace FurRealEngine
 {
     public partial class LoginGUI : Form
     {
+
+        //Public vars
+        static public string username;
+        static public string password;
+        static public string loginStatus; //Admin or User?
+
         public LoginGUI()
         {
             InitializeComponent();
+        }
+
+        private void LoginGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Cancel
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Submit
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string uname = textBox1.Text;
+            string pass = textBox2.Text;
+
+            username = uname; //Sets var username to the value in textBox1.
+            password = pass; //Sets var password to the value in textBox2.
+
+
+
+            //Users login status//
+
+            // Temp hardcode to determine it the user is a user or an admin.
+
+            //User Login Credentials
+            string unameUser = "user";
+            string passUser = "password";
+
+            //Admin Login Credentials
+            string unameAdmin = "admin";
+            string passAdmin = "password";
+
+            //If USER
+            if (String.Equals(username, unameUser) && String.Equals(password,passUser))
+            {
+
+            }
+
+            //If ADMIN
+            else if (String.Equals(username, unameAdmin) && String.Equals(password, passAdmin))
+            {
+
+            }
+
+            //If not a User or Admin 
+
+
+
+            //Zach - Add the onClick command to load the next GUI HERE!.
+
+
+        }
+
+        //Remember Password
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
