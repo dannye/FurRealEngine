1f452bd4a950c4e81cb26e45b078aa5c4bbdfba8
Ethan Wright <ethwrig@siue.edu>
2016-10-24 22:39:42 -0500

    LoginGUITest added

--- FurRealEngine/FurRealEngine/LoginGUI.cs
+++ FurRealEngine/FurRealEngine/LoginGUI.cs
@@ -109,6 +109,19 @@ namespace FurRealEngine
 
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ FurRealEngine/FurRealEngine/TestClasses/LoginGUITest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FurRealEngine
+{
+    [TestClass]
+    public class LoginGUITest : LoginGUI
+    {
+
+        [TestMethod]
+        public void emptyusernameFieldTest()
+        {
+            // arrange
+            string errorMessage = "Username field has been left blank.";
+            string blank = "";
+
+
+            // act
+            if (String.Equals(username, blank))
+            {
+                //Error Message
+                //LoginGUI.errorLabel.Text = errorMessage;
+
+            }
+
+            // assert
+            //Assert.AreEqual(LoginGUI.errorLabel.Text, blank);
+        }
+
+        [TestMethod]
+        public void emptyPasswordFieldTest()
+        {
+            // arrange
+            string errorMessage = "Password field has been left blank.";
+            string blank = "";
+
+
+            // act
+            if (String.Equals(password, blank))
+            {
+                //Error Message
+                //LoginGUI.errorLabel.Text = errorMessage;
+
+            }
+
+            // assert
+            //Assert.AreEqual(LoginGUI.errorLabel.Text, blank);
+        }
+    }
+}
