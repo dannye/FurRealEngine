d834566386c8fe23dbbd2348944a1549b49b57ad
zfenton <zfenton@siue.edu>
2016-10-24 20:17:23 -0500

    Added tests folder and a test class for config controller

---
+++ FurRealEngine/FurRealEngine/TestClasses/ConfigControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FurRealEngine
+{
+    [TestClass]
+    public class ConfigControllerTests
+    {
+
+        [TestMethod]
+        public void test()
+        {
+            
+        }
+
+    }
+}
