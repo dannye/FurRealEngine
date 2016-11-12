2a7849761077f83633489b832bade2f3d464ffb9
Ethan Wright <ethwrig@siue.edu>
2016-11-11 21:17:22 -0600

    Added test cases to ModifyAccountsTest.csto check if either fields are ledft blank or not.

---
+++ FurRealEngine/FurRealEngine/TestClasses/ModifyAccountsTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FurRealEngine.TestClasses
+{
+    [TestClass]
+    class ModifyAccountsTest : ModifyAccountsGUI
+    {
+
+
+        //Test is the suername fields are left blank or not.
+        [TestMethod]
+        public void emptyusernameFieldTest()
+        {
+            // arrange
+           string blank = "";
+
+
+            // act
+            if (String.Equals(ModifyAccountsGUI.currUname, blank))
+            {
+                ///Error Message
+                //MessageBox.Show("Username field has been left blank!");
+
+            }
+
+            // assert
+            //Assert.AreEqual(username, blank);
+        }
+
+        //Test if the password fields are left blank or not.
+        [TestMethod]
+        public void emptyPasswordFieldTest()
+        {
+            // arrange
+            string blank = "";
+
+
+            // act
+            if (String.Equals(ModifyAccountsGUI.currPass, blank))
+            {
+                //Error Message
+                //MessageBox.Show("Password field has been left blank!");
+
+            }
+
+            // assert
+            //Assert.AreEqual(password, blank);
+        }
+    }
+}
