1c92c5168356d96d31433b64ca2a4e2f0a1a06ec
Ethan Wright <ethwrig@siue.edu>
2016-12-08 23:51:53 -0600

    verify user accounts done and ready for testing by me. When I'm done I'll open it for public test.

--- FurRealEngine/FurRealEngine/LoginGUI.cs
+++ FurRealEngine/FurRealEngine/LoginGUI.cs
@@ -17,6 +17,7 @@ namespace FurRealEngine
         public string username;
         public string password;
         public string loginStatus; //Admin or User?
+        private LoginController loginController;
 
         public LoginGUI()
         {
@@ -26,49 +27,52 @@ namespace FurRealEngine
         //Submit
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
+            loginController = new LoginController();
             string uname = textBox1.Text;
             string pass = textBox2.Text;
 
-            username = uname; //Sets var username to the value in textBox1.
-            password = pass; //Sets var password to the value in textBox2.
+            loginController.verifyAccount(uname, pass);
+
+            //username = uname; //Sets var username to the value in textBox1.
+            //password = pass; //Sets var password to the value in textBox2.
 
 
 
-            //Users login status//
+            ////Users login status//
 
-            // Temp hardcode to determine it the user is a user or an admin.
+            //// Temp hardcode to determine it the user is a user or an admin.
 
-            //User Login Credentials
-            string unameUser = "user";
-            string passUser = "password";
+            ////User Login Credentials
+            //string unameUser = "user";
+            //string passUser = "password";
 
-            //Admin Login Credentials
-            string unameAdmin = "admin";
-            string passAdmin = "password";
+            ////Admin Login Credentials
+            //string unameAdmin = "admin";
+            //string passAdmin = "password";
 
-            //If USER
-            if (String.Equals(username, unameUser) && String.Equals(password, passUser))
-            {
+            ////If USER
+            //if (String.Equals(username, unameUser) && String.Equals(password, passUser))
+            //{
 
-                //Zach - This is if they are a user.
-                //Display next GUI..
-                ConfigGUI configGui = new ConfigGUI();
-                Hide();
-                configGui.initConfigGui(new User(uname, passUser, false));
-            }
+            //    //Zach - This is if they are a user.
+            //    //Display next GUI..
+            //    ConfigGUI configGui = new ConfigGUI();
+            //    Hide();
+            //    configGui.initConfigGui(new User(uname, passUser, false));
+            //}
 
-            //If ADMIN
-            else if (String.Equals(username, unameAdmin) && String.Equals(password, passAdmin))
-            {
+            ////If ADMIN
+            //else if (String.Equals(username, unameAdmin) && String.Equals(password, passAdmin))
+            //{
 
-                //Zach - This is if they are an Admin.
-                //Display next GUI..
-                ConfigGUI configGui = new ConfigGUI();
-                Hide();
-                configGui.initConfigGui(new User(uname, passUser, true));
-            }
+            //    //Zach - This is if they are an Admin.
+            //    //Display next GUI..
+            //    ConfigGUI configGui = new ConfigGUI();
+            //    Hide();
+            //    configGui.initConfigGui(new User(uname, passUser, true));
+            //}
 
-            //If not a User or Admin 
+            ////If not a User or Admin 
 
 
 
@@ -79,6 +83,11 @@ namespace FurRealEngine
 
         //Remember Password
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
